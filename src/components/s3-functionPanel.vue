f9a324960cb37b17b1f700b465f593edebf3fd4d
peiyao <952568789@qq.com>
2018-03-21 09:54:56 +0800

    添加九宫格组件---10%

---
+++ src/components/s3-functionPanel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="s3-functionPanel">
+    <ul>
+      <li v-for="(item,index) in modules" >
+        <router-link :to="item.pathUrl" solt="" tag="span">
+          <i class="iconfont" :class="item.icon"></i>
+          <p v-text="item.text" ></p>
+        </router-link>
+      </li>
+      <li v-for="(item,index) in otherModules" >
+        <router-link :to="item.pathUrl"  tag="span">
+          <i class="iconfont" :class="item.icon"></i>
+          <p v-text="item.text" ></p>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "s3-functionPanel",
+    props:{
+      modules: {
+        type: Array,
+      },
+      otherModules:{
+        type: Array,
+        default:function () {
+          return []
+        }
+      }
+    },
+    data(){
+      return{
+
+      }
+    },
+    created(){
+
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,15 @@
   <div>
     <s3-banner :images = 'images'></s3-banner>
     <div id="grid">
-      <ul>
-        <li v-for="(item,index) in gridlist" >
-          <router-link :to="item.pathUrl" solt="" tag="span">
-            <i class="iconfont" :class="item.icon"></i>
-            <p v-text="item.text" ></p>
-          </router-link>
-        </li>
-      </ul>
+      <s3-functionpanel :modules="gridlist" :otherModules="otherModules"></s3-functionpanel>
     </div>
   </div>
 </template>
 <script>
   //banner
   import s3Banner from '@/components/s3-banner.vue'
+  //九宫格
+  import s3Functionpanel from '@/components/s3-functionPanel.vue'
 
   export default {
     name: "home",
@@ -27,16 +22,19 @@
           {icon:'icon-guanxi',text:'团购',pathUrl:'/orderlist'}
         ],
         images:[],
+        otherModules:[]
       }
     },
     components: {
-      s3Banner
+      s3Banner,
+      s3Functionpanel
     },
     created(){
       s3.ajax('/getBanner',{},'s3core')
         .then(res =>{
           this.images = res.images;
         })
+      //获得特色九宫格数据
     },
   }
 </script>
